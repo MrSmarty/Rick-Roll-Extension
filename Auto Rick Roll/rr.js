27fc81cb89578654dd5067ff47ba46fb431ad71c
Lincoln <mrsmartymcfarty@gmail.com>
2021-10-25 22:08:07 -0500

    Add files via upload

---
+++ Auto Rick Roll/rr.js
@@ -0,0 +1,6 @@
+var arr = [];
+var l = document.links;
+for(var i = 0; i < l.length; i++) {
+  // l[i].href = "https://www.youtube.com/watch?v=QtBDL8EiNZo";
+  l[i].href = "https://www.youtube.com./watch?v=dQw4w9WgXcQ";
+}
